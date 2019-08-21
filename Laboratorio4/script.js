ebd607672245ebc94a7814880b570535e76fe82f
Alberto <albcastar@gmail.com>
2019-08-20 20:48:59 -0500

    Agregado segunda funcion de lab4

--- Laboratorio4/script.js
+++ Laboratorio4/script.js
@@ -1,6 +1,6 @@
 function tabla_potencias(limite = 10) {
 
-    let resultado = "<table><tbody>";
+    let resultado = '<table align="center"><tbody>';
     let potencia = 2;
     resultado += "<caption>Tabla de numeros al cuadrado y al cubo</caption>"
     for (let i = 1; i <= potencia + 1; i++) {
@@ -15,7 +15,26 @@ function tabla_potencias(limite = 10) {
     resultado += "</tbody></table><hr/>";
     return resultado;
 }
+function tiempo_en_segundos(tiempo_inicial,tiempo_final)
+{
+    return (tiempo_final - tiempo_inicial)/1000;
+}
 
+//Ejercicio 1
 let limite_usuario = prompt("Ingresa un numero: ");
 let tabla_personalizada = tabla_potencias(limite_usuario);
 document.write(tabla_personalizada);
+
+//Ejercicio 2
+let numero_aleatorio_1 = Math.floor(Math.random() * 500);
+let numero_aleatorio_2 = Math.floor(Math.random() * 500);
+
+let resultado = numero_aleatorio_1 + numero_aleatorio_2;
+
+let t0 = performance.now();
+let respuesta_usuario = prompt("Ingresa el resultado de " + numero_aleatorio_1 + " + " + numero_aleatorio_2);
+let t1 = performance.now();
+
+
+document.write(respuesta_usuario == resultado ? "Tu respuesta es correcta<hr/>" : "Tu respuesta es incorrecta<br>");
+document.write("Tu tiempo de respuesta fue de " + tiempo_en_segundos(t0,t1) + " segundos</hr>");
